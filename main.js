55e1581ec314b7e45d4e26d550d7f3eab2d97395
Melisa <melisa@gredu.asia>
2020-03-14 17:05:53 +0700

    Fix Responsiveness Ipad, Mobile

--- main.js
+++ main.js
@@ -57,18 +57,18 @@ var TxtRotate = function(el, toRotate, period) {
   };
 
 // /* Profile Modal */
-//  var modal = document.getElementById("profile");
-// var trigger = document.getElementById("open");
-// var close = document.getElementById('close');
-// var black = document.getElementsByClassName('black');
+ var modal = document.getElementById("profile");
+var trigger = document.getElementById("open");
+var close = document.getElementById('close');
+var black = document.getElementsByClassName('black');
 
-// trigger.onclick = function() {
-//     modal.style.display = "block";
-//     close.style.display = "block";
-//     black.style.display = "none";
-// }
+trigger.onclick = function() {
+    modal.style.display = "block";
+    close.style.display = "block";
+    black.style.display = "none";
+}
 
-// close.onclick = function() {
-//     modal.style.display = "none";
-//     close.style.display = "none";
-// }
+close.onclick = function() {
+    modal.style.display = "none";
+    close.style.display = "none";
+}
